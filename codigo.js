e02ecd187b1be20b7fd1469c6998e3332853a273
AlvaroJimenez13 <66470657+AlvaroJimenez13@users.noreply.github.com>
2021-01-29 10:15:12 +0100

    actualizador

--- codigo.js
+++ codigo.js
@@ -1,10 +1,19 @@
 //FUNCION PARA PROBAR INICIO DE SESION
 "use strict";
+function empezar(){
+    navegador.style.display="none";
+    divFrmAltaAlumno.style.display="none";
+    divFrmAltaTutor.style.display="none";
+    divFrmAltaGrupo.style.display="none";
+}
 function iniciar()
 {
     if (document.getElementById ("txtContraseña").value=='usuario' && document.getElementById("txtCorreo").value=='usuario')
     { 
         navegador.style.display="block";
+        divFrmAltaAlumno.style.display="none";
+        divFrmAltaTutor.style.display="none";
+        divFrmAltaGrupo.style.display="none";
     } 
     else{ 
          alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
