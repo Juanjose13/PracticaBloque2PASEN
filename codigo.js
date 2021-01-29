d3d7efbd9e8d7146277a96c9817495d15c3a761d
AlvaroJimenez13 <66470657+AlvaroJimenez13@users.noreply.github.com>
2021-01-29 09:44:00 +0100

    intento

--- codigo.js
+++ codigo.js
@@ -4,9 +4,20 @@ function iniciar()
 {
     if (document.getElementById ("txtContraseña").value=='usuario' && document.getElementById("txtCorreo").value=='usuario')
     { 
-        document.getElementById("frmInicio").submit(); 
+        document.getElementById("navegador").removeAttribute("hidden");
+        document.getElementById("navegador").removeAttribute("hidden");
+        document.getElementById("navegador").removeAttribute("hidden");
+        // document.getElementById("frmInicio").submit(); 
     } 
     else{ 
          alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
     } 
+}
+
+
+function altaAlumno(){
+
+    divFrmAltaAlumno.style.display="block";
+    document.getElementById("divFrmAltaAlumno").removeAttribute("hidden");
+
 }
