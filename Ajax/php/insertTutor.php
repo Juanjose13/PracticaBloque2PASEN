510b9bef672c48ea94ae1bc936df5060b97aa31c
AlvaroJimenez13 <66470657+AlvaroJimenez13@users.noreply.github.com>
2021-02-17 19:33:25 +0100

    insert

--- Ajax/php/insertTutor.php
+++ Ajax/php/insertTutor.php
@@ -1,3 +1,34 @@
 <?php
+////////////////// INSERTAR TUTORES
 
+// Configuración BASE DE DATOS MYSQL
+$servidor  = "localhost";
+$basedatos = "pasen";
+$usuario   = "root";
+$password  = "";
+
+// $dani = dni alumno;
+// $nombre = nombre del alumno;
+// $apellido = apellidos alumnos;
+// $edad = edad ;
+
+// Creamos la conexión al servidor.
+$conexion = mysqli_connect($servidor, $usuario, $password,$basedatos) or die(mysqli_error($conexion));
+mysqli_query($conexion,"utf8");
+
+// Consulta SQL para obtener los datos de los centros.
+$sql = "INSERT INTO alumnos (dniTutor, nombreTutor, asignatura) VALUES ($dni,'$nombre','$asignatura',);";
+$resultado = mysqli_query($conexion,$sql);
+
+if ($resultado){
+    $respuesta["error"] = 0;
+    $respuesta["mensaje"] = "Alta realizada"; 
+} else {
+    $respuesta["error"] = 1;
+    $respuesta["mensaje"] = "Error en el proceso de alta: ".mysqli_error($conexion);
+}
+mysqli_close($conexion);
+
+
+echo json_encode($respuesta);
 ?>
